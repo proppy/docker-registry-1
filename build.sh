05016b55784ac85490b30a6edf4a9b50e92884e6
Johan Euphrosine <proppy@google.com>
2014-04-08 14:52:54 -0700

    build: optimize image size

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 set -e
 
 CONTEXT_PATH=$1
+export DEBIAN_FRONTEND=noninteractive
 
 apt-get update || true
 apt-get install -y --no-install-recommends \
@@ -10,10 +11,11 @@ apt-get install -y --no-install-recommends \
     python-yaml python-lzma python-crypto python-requests \
     python-simplejson python-redis python-openssl wget python-pip
 rm /var/lib/apt/lists/*_*
+apt-get clean
 git clone https://github.com/dotcloud/docker-registry.git /docker-registry
 (cd /docker-registry && pip install -r ${CONTEXT_PATH}/requirements.txt)
-apt-get purge -y --force-yes gcc python-dev liblzma-dev \
-    libpython2.7-dev gcc-4.7 libc6-dev \
-    linux-libc-dev git-core
+apt-get purge -y --force-yes gcc python-dev git-core
+apt-get autoremove -y --force-yes
+
 cp ${CONTEXT_PATH}/run.sh /docker-registry/run.sh
 cp ${CONTEXT_PATH}/config.yml /docker-registry/config/config.yml
